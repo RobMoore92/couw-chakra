38acd80952483d6ede2746de02c0b84fdeb01974
RWMoo <contract@robertmoorewebdev.com.au>
2022-03-03 15:01:18 +0930

    fdsfdfg

--- components/Drawer.jsx
+++ components/Drawer.jsx
@@ -1,42 +1,85 @@
 import React from "react";
 import {
-    Button,
-    Drawer, DrawerBody,
-    DrawerCloseButton,
-    DrawerContent, DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay, Input,
-    useDisclosure
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Input,
+  MenuItem,
+  MenuList,
+  useDisclosure,
+  VStack,
 } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 
-const DrawerMenu = ({isOpenDrawer, onOpenDrawer, onCloseDrawer}) => {
+const DrawerMenu = ({ isOpenDrawer, onOpenDrawer, onCloseDrawer }) => {
+  const router = useRouter();
+  return (
+    <>
+      <Drawer isOpen={isOpenDrawer} placement="left" onClose={onCloseDrawer}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
 
-    return(
-        <>
-            <Drawer
-                isOpen={isOpenDrawer}
-                placement='left'
-                onClose={onCloseDrawer}
-            >
-                <DrawerOverlay />
-                <DrawerContent>
-                    <DrawerCloseButton />
-                    <DrawerHeader>Create your account</DrawerHeader>
-
-                    <DrawerBody>
-                        <Input placeholder='Type here...' />
-                    </DrawerBody>
-
-                    <DrawerFooter>
-                        <Button variant='outline' mr={3} onClick={onCloseDrawer}>
-                            Cancel
-                        </Button>
-                        <Button colorScheme='blue'>Save</Button>
-                    </DrawerFooter>
-                </DrawerContent>
-            </Drawer>
-        </>
-    )
-}
+          <DrawerBody>
+            <VStack align={"flex-start"}>
+              <Button
+                onClick={() => {
+                  router.push("/");
+                }}
+                isFullWidth
+                colorScheme="teal"
+                size="md"
+              >
+                Home
+              </Button>
+              <Button
+                onClick={() => {
+                  router.push("/jobs");
+                }}
+                isFullWidth
+                colorScheme="teal"
+                size="md"
+              >
+                Jobs
+              </Button>
+              <Button
+                onClick={() => {
+                  router.push("/clients");
+                }}
+                isFullWidth
+                colorScheme="teal"
+                size="md"
+              >
+                Clients
+              </Button>
+              <Button
+                onClick={() => {
+                  router.push("/items");
+                }}
+                isFullWidth
+                colorScheme="teal"
+                size="md"
+              >
+                Items
+              </Button>
+            </VStack>
+          </DrawerBody>
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onCloseDrawer}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
 
 export default DrawerMenu;

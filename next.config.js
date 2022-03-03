2d48cc9ecc1a6e4191269e53dbf6b29727fed2e4
RWMoo <contract@robertmoorewebdev.com.au>
2022-03-03 14:47:01 +0930

    fdsfdfg

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 const withPWA = require("next-pwa");
-const optimizedImages = require("next-optimized-images");
 const prod = process.env.NODE_ENV === "production";
-const runtimeCaching = require("next-pwa/cache");
 module.exports = withPWA({
   pwa: {
     disable: !prod,
@@ -9,8 +7,6 @@ module.exports = withPWA({
   },
   reactStrictMode: true,
   images: {
-    loader: "custom",
-    path: "images/",
     domains: ["cdn.pixabay.com", "firebasestorage.googleapis.com"],
   },
 });
